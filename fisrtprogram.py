f4f8ff534d2f5bc2e8925cd4760681a08995aad5
Harsh Tiwari <harsh.tiwari7112@gmail.com>
2025-08-05 02:39:49 +0530

    fisrtprogram.py
    I improve the code with learning pyhon in these few months!!

--- fisrtprogram.py
+++ fisrtprogram.py
@@ -2,35 +2,44 @@ class Account:
     def __init__(self, acc_no, bal):
         self.balance = bal
         self.account_no = acc_no
-    
+    dict = {
+         1 : "Debit",
+         2 : "Credit",
+         3 : "Get Balance",
+         4 : "Exit"
+    }
+
     def holder(self,acc_holder):
             self.account_holder = acc_holder
-            print(f"Account number:{self.account_no} is belong to {self.account_holder}")
 
     def debit(self,amount):
         self.balance -= amount
-        print(f"Debited amount{amount}")
-        print(f"Remaining balance is {self.balance}")
+        print(f"Debited amount: {amount}")
+        print(f"Total remaining balance in account {self.account_no} is {self.get_balance()}")
     
     def credit(self,amount):
         self.balance += amount
-        print(f"Credited amount {amount}")
-        print(f"Remaining balance is {self.balance}")
-    
+        print(f"Credited amount: {amount}")
+        print(f"Total remaining balance in account {self.account_no} is {self.get_balance()}")
+
     def get_balance(self):
         return self.balance
 
 acc1 = Account(input("Enter account number:" ),int(input("Enter balance:" )))
 acc1.holder(input("please provide the name of account holder: "))
-acc1.debit(int(input("enter your debiting amount: ")))
-acc1.credit(int(input("Enter your crediting amount:" )))
-print(f"Total remaining balance in account {acc1.account_no} is {acc1.get_balance()}")
-print(f"THANKS FOR JOINING US")
-
-acc2 = Account(20000000, 707045)
-acc2.holder(input("please provide the name of account holder: "))
-acc2.debit(int(input("enter your amount" )))
-acc2.debit(60000)
-acc2.credit(100000)
-print(f"Total remaining balance in account {acc2.account_no} is {acc2.get_balance()}")
-print(f"THANKS FOR JOINING US")
+while True:
+    print(f"Welcome to the banking system {acc1.account_holder}")
+    for key, value in acc1.dict.items():
+        print(f"{key}. {value}")
+    i = int(input("Please select a value given above: "))
+    if i == 1:
+        acc1.debit(int(input("Enter your debiting amount: ")))
+    elif i == 2:
+        acc1.credit(int(input("Enter your crediting amount: ")))
+    elif i == 3:
+        print(f"Your current balance is {acc1.get_balance()}")
+    elif i == 4:
+        print("Thank you for using our banking system.")
+        break
+    else:
+        print("Invalid option selected.")
